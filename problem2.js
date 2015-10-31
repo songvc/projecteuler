a7a375f16b9ab8c5b0a244eae1b5559ebeddd82f
songvc@gmail.com <songvc@Vincents-Pro.attlocal.net>
2015-10-31 14:29:26 -0700

    problem2 done

--- problem2.js
+++ problem2.js
@@ -2,7 +2,7 @@ function fib(n){
 
 	if (n == 1){
 		return 1;
-	}else if (n==2){
+	}else if (n == 2){
 		return 2;
 	}else{
 		return fib(n-2) + fib(n-1);
@@ -10,5 +10,23 @@ function fib(n){
 
 }
 
+function findEvenSums(limit){
+	var n = 1,
+		sum = [];
+	
+	while (fib(n) < limit){
+		if (fib(n)%2==0){
+			sum.push(fib(n));
+		}
+		n++;
+	}
+
+	return sum.reduce(function(a,b){
+		return a+b;
+	})
+
+}
+
 console.log(fib(5));
 console.log(fib(6));
+console.log(findEvenSums(4000000));
