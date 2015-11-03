0a2a5c90ef8d4d62558c0254cf5a992a4726292a
Vincent Song <songvc@users.noreply.github.com>
2015-11-02 21:15:58 -0800

    problem 11 in progress

--- problem10.js
+++ problem10.js
@@ -1,35 +1,34 @@
 function findPrimes(limit){
 	var sequence=[],
-	upperbound = Math.sqrt(limit);
+	upperbound = Math.sqrt(limit),
+		output=[];
 	
 	//Using Sieve of Eratosthenes Algorithms
 
-	//create list of sequences up to limit
-	for (var i = 2; i < limit; i++){
-		sequence.push(i);
+	for (var i = 0; i < limit; i++){
+		sequence.push(true);
 	}
 
-
-	//find indexes of prime multiples and remove them
-	for (var i =2; i < upperbound; i++){
-		sequence[i]
-	}
-
-
-
-	for (var i = 0; i < upperbound; i++){
-		console.log(sequence.length);
-		for (var j = 2; j < limit/sequence[i]; j++){
-			console.log(sequence[i] + " " +j + " " + sequence[i]*j);
-			if(sequence.indexOf(sequence[i]*j)>1){
-				sequence.splice(sequence.indexOf(sequence[i]*j),1);
+	for (var i = 2; i <= upperbound; i++){
+		if(sequence[i]){
+			for(var j = 2 * i; j < limit; j+= i){
+				sequence[j] = false;
 			}
 		}
-	}		
+	}
 
-	return sequence.reduce(function(a,b){ 
+	for (var x = 2; x < limit; x++){
+		if(sequence[x]){
+			output.push(x);
+		}
+	}
+
+	console.log(output)
+
+	return output.reduce(function(a,b){ 
 		return a+b;
 	});
 }
 
+// console.log(findPrimes(100));
 console.log(findPrimes(2000000));
