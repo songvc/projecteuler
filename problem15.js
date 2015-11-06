0c5e28dd053010bdb27cc1533e8c7219d23b3c40
Vincent Song <songvc@users.noreply.github.com>
2015-11-05 22:49:16 -0800

    problem 15 done

--- problem15.js
+++ problem15.js
@@ -1,3 +1,16 @@
 function latticePaths(row, col){
-	
+	// if 2 x 2 -> 4 chooses 2 
+	// if 3 x 3 -> 6 chooses 3 
+	var denominator = 1,
+		numerator = 1;
+
+	for (var i = 1; i <= row + col; i++){
+		denominator *= i;
+	}
+	for (var j = 1; j <= row; j++){
+		numerator *= j;
+	}
+	return denominator/(numerator*numerator);
 }
+
+console.log(latticePaths(20,20));
