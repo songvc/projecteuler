2670d44296d071910c205ed88f07b186ddc3f727
Vincent Song <songvc@users.noreply.github.com>
2015-11-07 16:53:13 -0800

    problem 16 done

--- problem16.js
+++ problem16.js
@@ -1,18 +1,43 @@
-function power_digit_sum(base,exp){
-	var str = Math.pow(base,exp).toString(),
-		sum = 0;
+function power_digit_sum(base){
+	var number = [1],
+	    sum = 0;
 
-	console.log(str);
-	for (var i = 0; i < str.length; i++){
-		sum += parseInt(str[i]);
+	for(var i = 0; i < 1000; i++)
+	{
+	    var overflow = 0,
+	        count = number.length + 1
+
+	    for(var j = 0; j < count; j++)
+	    {
+	        var digit = number[j] || 0;
+
+	        digit = 2 * digit + overflow;
+
+	        if(digit > 9)
+	        {
+	            digit -= 10;
+	            overflow = 1;
+	        }
+	        else
+	        {
+	            overflow = 0;
+	        }
+
+	        number[j] = digit;
+	    }
 	}
 
-	return sum;
+	for(var i = 0; i < 1000; i++)
+	{
+	    sum += number[i];
+	}
+
+	console.log(sum);
+
 }
 
-for (var i =1; i < 100; i++){
-	console.log(i + "  " + Math.pow(2,i))
-}
 
+
+power_digit_sum(2);
 
 
